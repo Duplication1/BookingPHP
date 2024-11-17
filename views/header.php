ae76db7aa7fa823f5ef7c2052d2a7d1e966ecf71
Duplication1 <gamot.kim.fernandez@gmail.com>
2024-11-17 22:42:34 +0800

    header changes

--- views/header.php
+++ views/header.php
@@ -1,37 +1,47 @@
 <header class="main-header">
-  <nav class="navbar navbar-expand-lg navbar-light  main-nav-bar">
-    <div class="container-fluid">
-      <!-- Logo Section -->
-        <div class="header-logo-container">
-            <a class="navbar-brand d-flex align-items-center" href="#">
-            <img src="../images/logo.png" alt="Dental Clinic Logo" height="70">
-            <span class="ms-2">Dental Clinic</span>
-            </a>
-        </div>
-      <!-- Toggler for Collapsible Navigation -->
-      <button class="navbar-toggler main-nav-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand d-flex align-items-center" href="#">
+        <img src="../images/logo.png" alt="Dental Clinic Logo" height="50">
+        <span class="ms-2 fw-bold color-white">WILERICH OPT. CLINIC</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-
-      <!-- Navigation Links -->
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav  mb-2 mb-lg-0 main-ul-nav">
+      <div class="collapse navbar-collapse" id="navbarContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" href="index.php">Home</a>
+            <a class="nav-link color-white" href="#">EYEGLASSES</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="about.php">About</a>
+            <a class="nav-link color-white" href="#">CONTACT LENS</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="pricing.php">Pricing</a>
+            <a class="nav-link color-white" href="#">SERVICES</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="appointment.php">Appointment</a>
-          </li>
-          <button class="btn btn-primary me-2 signup-btn">Sign Up</button>
-          <button class="btn btn-outline-primary login-btn">Login</button>
         </ul>
+        <div class="d-flex gap-2 align-items-center right-side-nav">
+        <button class="btn color-white book-an-appointment-btn"><img src="../images/book-an-appointment-icon.png"/>Book An Appointment</button>
+        <div class="white-vertical-lines"></div>
+        <button class="btn"><img src="../images/map-link-icon.png"/></button> 
+        <div class="white-vertical-lines"></div>
+          <!-- Search Button -->
+          <button id="searchButton" class="btn" type="button">
+            <img src="../images/search-icon.png"/>
+          </button>
+        <div class="white-vertical-lines"></div>
+          <!-- Dropdown Button -->
+          <div class="dropdown">
+            <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="../images/profile-icon.png" />
+            </button>
+            <ul class="dropdown-menu color-white" aria-labelledby="dropdownMenuButton">
+              <li><a class="dropdown-item" href="#">Login</a></li>
+              <li><a class="dropdown-item" href="#">Sign Up</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </header>
---
+++ views/script.js
@@ -0,0 +1,7 @@
+// Add the 'show' class to trigger animation when dropdown is opened
+const dropdownButton = document.querySelector('#dropdownMenuButton');
+dropdownButton.addEventListener('click', function() {
+  const dropdown = dropdownButton.closest('.dropdown');
+  dropdown.classList.toggle('show');
+});
+
